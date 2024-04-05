c20b65309608910071ba271437e4ced418d99a37
shruthi reddy <shruthireddy583@gmail.com>
2024-04-05 10:58:40 +0530

    .

---
+++ python Data types/two.py.py
@@ -0,0 +1,14 @@
+size=[38,40,42]
+t=(38,40,42)
+ids={101,102,103}
+emp={'id':101,'name':'Rahul'}
+
+
+print(size)
+print(type(size))
+print(t)
+print(type(t))
+print(ids)
+print(type(ids))
+print(emp)
+print(type(emp))
