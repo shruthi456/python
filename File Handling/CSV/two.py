70452467072c302da18c2258fed356d08f659fe5
shruthi reddy <shruthireddy583@gmail.com>
2024-05-06 12:47:08 +0530

    .

---
+++ File Handling/CSV/two.py
@@ -0,0 +1,11 @@
+#read data.csv file and print data
+
+import csv
+fp=open('data.csv','r')
+user_csv_obj=csv.reader(fp)
+users=list(user_csv_obj)
+
+for user in users:
+    for data in user:
+        print(data)
+fp.close()        
