1a6cc821d964f823d1ef45a31714b02a2a37edce
shruthi reddy <shruthireddy583@gmail.com>
2024-05-10 10:52:10 +0530

    .

---
+++ oops/oops3/four.py
@@ -0,0 +1,10 @@
+class Account:
+    def open_Account(self):
+        print("Account opened successfully")
+
+    def deposit_Account(self,amount):
+        print("Amount Deposited Succesfully")
+
+a1=Account(101,"Rahul",rahul@gmail.com)
+a1.open_Account()
+a1.deposit_Account(5000)        
