de6290d04667de8f66054e55867b4ff9323c4919
shruthi reddy <shruthireddy583@gmail.com>
2024-05-10 10:44:52 +0530

    .

---
+++ oops/oops3/two.py
@@ -0,0 +1,8 @@
+class Account:
+    def __init__(self,a,b,c):
+        pass
+    def deposit_Account(self,amount):
+        pass
+
+a1=Account(101,"Shruthi",5000)
+a1.deposit_Account(5000)            
