e29ef521093f7bd50ad3a6fc24be1e4a34e2ebfd
shruthi reddy <shruthireddy583@gmail.com>
2024-05-17 12:35:16 +0530

    .

---
+++ oops/4 pillers/encapsulation/one.py
@@ -0,0 +1,10 @@
+class Employee:
+    org_name="TCS"
+    def set_eid(self,eid):
+        self.eid=id
+    def get_eid(self):
+        return self.eid   
+
+
+print (Employee)  
+print (Employee.__dict__)       
