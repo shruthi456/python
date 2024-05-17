51d794337456a44c133a4762c0832b5a9b2f36ce
shruthi reddy <shruthireddy583@gmail.com>
2024-05-17 11:46:13 +0530

    .

---
+++ oops/instance methods/classmethod.py
@@ -0,0 +1,14 @@
+class Account:
+    min_bal=500
+    @classmethod
+    def get_min_bal(cls):
+      return cls.min_bal
+    @classmethod
+    def update_min_bal(cls):
+      cls.min_bal=700
+
+print(Account.get_min_bal())
+Account.update_min_bal()
+print(Account.get_min_bal())
+
+
