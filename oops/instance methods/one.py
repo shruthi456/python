7d48426b164b4e0cb043022668c97cdd61a39d1e
shruthi reddy <shruthireddy583@gmail.com>
2024-05-17 11:35:19 +0530

    .

---
+++ oops/instance methods/one.py
@@ -0,0 +1,14 @@
+class Account:
+    def set_account_Id(self,id):
+        self.acc_id=id
+
+    def get_account_Id(self):
+        return self.acc_id
+
+a1=Account()
+a2=Account()
+
+a1.set_account_Id(101)
+a2.set_account_Id(102)
+print(a1.get_account_Id())
+print(a2.get_account_Id())        
